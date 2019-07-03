129812b7d791596d598e6abac1de5dd33195083e
符凌云 <1394327107@qq.com>
2019-07-03 11:33:09 +0800

    LitePal

---
+++ app/src/main/java/com/example/softd/yichun201907/DB/Account.java
@@ -0,0 +1,26 @@
+package com.example.softd.yichun201907.DB;
+
+import org.litepal.crud.LitePalSupport;
+
+public class Account extends LitePalSupport {
+
+    String name = "";
+
+    String password = "";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
